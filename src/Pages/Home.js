42cf0e8b449a2dcaceb0120cfd80af11190b8b78
Thomas C <carrt313@gmail.com>
2021-05-09 14:41:03 -0400

    it work

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,6 +2,7 @@ import { useState, useRef, useEffect } from 'react'
 import PageTemplate from './Template'
 import Webcam from 'react-webcam'
 import axios from 'axios'
+import FormData from 'form-data'
 import '../css/Home.css';
 
 function App() {
@@ -12,29 +13,29 @@ function App() {
         async function callPost() {
             let im = webcamRef.current.getScreenshot()
             let formData = new FormData()
-            let image = new Image()
-            image.src = im
-            formData.append('image', image)
+            if (!im) { console.log(im); return }
+            var block = im.split(";");
+            var contentType = block[0].split(":")[1];
+            var realData = block[1].split(",")[1];
+            var blob = b64toBlob(realData, contentType);
+            formData.append('image', blob)
             const response = await axios({
-                method: "post",
-                url: "http://a.bluebounty.tech:9000/predict",
-                data: formData,
+                method: 'post',
+                url: 'http://a.bluebounty.tech:9000/predict',
+                mode: 'cors',
                 headers: {
-                    'Content-Type': `multipart/form-data; boundary=${formData._boundary}`,
+                    'Access-Control-Allow-Origin': '*'
                 },
+                data: formData
+            }).catch(er => {
+                console.log('error: ', er)
             })
-            // const response = await axios.post('http://a.bluebounty.tech:9000/predict', formData, {
-            //     mode: 'cors',
-            //     headers: {
-            //         'Access-Control-Allow-Origin': '*'
-            //     }
-            // })
-            const data = await response.json();
+            const data = response.data
             console.log(data)
+            if(!data) return
             setData(data);
         }
-        callPost()
-        setInterval(callPost, 2500)
+        setInterval(callPost, 2000)
     })
     return (
         <>
@@ -62,3 +63,28 @@ function App() {
 }
 
 export default App;
+
+
+function b64toBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+    }
+
+    var blob = new Blob(byteArrays, { type: contentType });
+    return blob;
+}
