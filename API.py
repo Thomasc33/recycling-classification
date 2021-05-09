c02689ce5a12848c79b3b8037b713f3511b8f86d
Thomas C <carrt313@gmail.com>
2021-05-09 00:41:33 -0400

    api

---
+++ API.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from fastai.basic_train import load_learner
+from fastai.vision import open_image
+from flask_cors import CORS,cross_origin
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+# load the learner
+learn = load_learner(path='./models', file='trained_model.pkl')
+classes = learn.data.classes
+
+
+def predict_single(img_file):
+    'function to take image and return prediction'
+    prediction = learn.predict(open_image(img_file))
+    probs_list = prediction[2].numpy()
+    return {
+        'category': classes[prediction[1].item()],
+        'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
+    }
+
+
+# route for prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    return jsonify(predict_single(request.files['image']))
+
+if __name__ == '__main__':
+    app.run()
