5da412a6ff36be91e5830901deb467bfa623ba77
Thomas C <carrt313@gmail.com>
2021-05-09 02:00:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask_cors import CORS
 from fastai.vision.all import load_learner, PILImage
 from pathlib import PurePosixPath
 import re
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
@@ -11,13 +14,9 @@ def get_y(x):
   y =  list(x for x in pattern.search(str(x)).groups())
   return y
 
-
-
 # load the learner
 learn = load_learner('trained_model.pkl')
-classes = learn.data.classes
-
-
+#classes = learn.data.classes
 
 def predict_single(img_file):
     'function to take image and return prediction'
@@ -26,7 +25,6 @@ def predict_single(img_file):
         'categories': [str(x) for x in pred],
         'probs': {c: round(float(probs[i]), 5) for (i, c) in enumerate(classes)}
     }
-
 
 # route for prediction
 @app.route('/predict', methods=['POST'])
