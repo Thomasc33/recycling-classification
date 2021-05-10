058a0d39c448e117b7f18084f0959fcea4be0669
Tcarr9442 <35206339+Tcarr9442@users.noreply.github.com>
2021-05-09 18:56:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ learn_wadaba = load_learner('wadaba.pkl')
 # define get_y of next model
 learn_duck = load_learner('duck.pkl')
 
-def predict_single_wadaba(img_file, learn):
+def predict_single(img_file, learn):
     'function to take image and return prediction'
     pred, pred_idx, probs = learn.predict(PILImage.create(img_file))
     return {
@@ -33,12 +33,13 @@ def predict_single_wadaba(img_file, learn):
 @app.route('/predict', methods=['POST'])
 @cross_origin()
 def predict():
+    print(request.form, request.args)
     selected_model = request.form.get('model','wadaba')
     if selected_model == 'wadaba':
         return jsonify(predict_single(request.files['image'], learn_wadaba))  
     elif selected_model == 'duckduckgo':
         return jsonify(predict_single(request.files['image'], learn_duck))
-    
+    return "model not found", 400
 
 if __name__ == '__main__':
-    app.run(host= '192.168.254.76', port=9000, debug=False, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host= '192.168.254.76', port=9000, debug=True, ssl_context=('cert.pem', 'key.pem'))
